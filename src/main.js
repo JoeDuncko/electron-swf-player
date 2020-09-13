652124c3a4e414e3c9e98bcefba8de32b2771097
Joe Duncko <JoeDuncko@users.noreply.github.com>
2020-09-13 18:22:18 -0400

    Flash player is loading on mac

--- src/main.js
+++ src/main.js
@@ -1,46 +1,42 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+let mainWindow;
+
+app.on("window-all-closed", function () {
+  if (process.platform != "darwin") app.quit();
+});
+
+let ppapi_flash_path;
+
+// Specify flash path.
+// On Windows, it might be /path/to/pepflashplayer.dll
+// On OS X, /path/to/PepperFlashPlayer.plugin
+// On Linux, /path/to/libpepflashplayer.so
+if (process.platform == "win32") {
+  ppapi_flash_path = path.join(__dirname, "pepflashplayer.dll");
+} else if (process.platform == "linux") {
+  ppapi_flash_path = path.join(__dirname, "libpepflashplayer.so");
+} else if (process.platform == "darwin") {
+  ppapi_flash_path = path.join(
+    __dirname,
+    "/pepper/mac/PepperFlashPlayer.plugin"
+  );
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+app.commandLine.appendSwitch("ppapi-flash-path", ppapi_flash_path);
+
+// Specify flash version, for example, v18.0.0.203
+app.commandLine.appendSwitch("ppapi-flash-version", "18.0.0.203");
+
+app.on("ready", function () {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      plugins: true,
+      nodeIntegration: true,
+    },
   });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  mainWindow.loadURL("http://www.adobe.com/software/flash/about/");
 });
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
