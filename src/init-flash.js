971424700abd61cdaea6850bf8c2149176f16f5b
Joe Duncko <JoeDuncko@users.noreply.github.com>
2021-05-30 11:54:25 -0400

    refactor: update dependencies, run linter, remove extra dependencies (#10)
    * Remove style-loader
    
    * Remove node-loader
    
    * Update electron-forge, update eslint, add prettier, add `lint` command
    
    * Update to latest version of electron 11 (the last version to support flash)
    
    * Update `package-lock.json`

--- src/init-flash.js
+++ src/init-flash.js
@@ -1,25 +1,25 @@
-const { app } = require('electron');
-const path = require('path');
+const { app } = require("electron");
+const path = require("path");
 
 const getPlugin = () => {
-  if (process.platform === 'win32') {
-    if (process.arch === 'x64') {
+  if (process.platform === "win32") {
+    if (process.arch === "x64") {
       return {
-        pluginPath: 'pepflashplayer64_32_0_0_363.dll',
-        version: '32.0.0.363',
+        pluginPath: "pepflashplayer64_32_0_0_363.dll",
+        version: "32.0.0.363",
       };
     }
 
     return {
-      pluginPath: 'pepflashplayer32_32_0_0_363.dll',
-      version: '32.0.0.363',
+      pluginPath: "pepflashplayer32_32_0_0_363.dll",
+      version: "32.0.0.363",
     };
   }
 
-  if (process.platform === 'darwin') {
+  if (process.platform === "darwin") {
     return {
-      pluginPath: 'PepperFlashPlayer.plugin',
-      version: '30.0.0.127',
+      pluginPath: "PepperFlashPlayer.plugin",
+      version: "30.0.0.127",
     };
   }
 
@@ -36,9 +36,9 @@ module.exports = () => {
   const { pluginPath, version } = plugin;
 
   app.commandLine.appendSwitch(
-    'ppapi-flash-path',
-    path.join(__dirname, 'pepper-flash-player', pluginPath)
+    "ppapi-flash-path",
+    path.join(__dirname, "pepper-flash-player", pluginPath)
   );
 
-  app.commandLine.appendSwitch('ppapi-flash-version', version);
+  app.commandLine.appendSwitch("ppapi-flash-version", version);
 };
